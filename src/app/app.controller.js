27c8a493923484b57a6ff4d1854c00f9b593f207
Eric Gibby <eric.gibby@imaginelearning.com>
2017-09-12 10:44:13 -0600

    Added filters to AppController to modify model for experimentation with data-binding to child directives/components.

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -3,16 +3,21 @@
 
 	angular.module('app').controller('AppController', AppController);
 
-	AppController.$inject = ['$log', 'Data'];
-	function AppController($log, Data) {
+	AppController.$inject = ['$log', '$filter', '$scope', 'Data'];
+	function AppController($log, $filter, $scope, Data) {
 		var vm = this;
-		vm.data = undefined;
+
+		// Model
+		vm.data = [];
+		vm.displayData = [];
+		vm.numRows = 20;
+		vm.filter = '';
 		vm.columns = [
-			{name: 'id', display: 'ID'},
-			{name: 'firstName', display: 'First Name'},
-			{name: 'lastName', display: 'Last Name'},
-			{name: 'email', display: 'Email'}
-		]
+			{ name: 'id', display: 'ID' },
+			{ name: 'firstName', display: 'First Name' },
+			{ name: 'lastName', display: 'Last Name' },
+			{ name: 'email', display: 'Email' }
+		];
 
 		activate();
 
@@ -22,10 +27,35 @@
 			Data.get()
 				.then(function(data) {
 					vm.data = data;
+					populateDisplayData();
 				})
 				.catch(function(error) {
 					$log.error(error);
 				});
+
+			$scope.$watch(function() {
+				return vm.numRows;
+			}, onNumRowsChanged)
+
+			$scope.$watch(function() {
+				return vm.filter;
+			}, onFilterChanged);
+		}
+
+		function onNumRowsChanged(newValue, oldValue) {
+			if (newValue !== oldValue) {
+				populateDisplayData();
+			}
+		}
+
+		function onFilterChanged(newValue, oldValue) {
+			if (newValue !== oldValue) {
+				populateDisplayData();
+			}
+		}
+
+		function populateDisplayData() {
+			vm.displayData = $filter('filter')(vm.data, vm.filter).slice(0, vm.numRows);
 		}
 	}
 })();
