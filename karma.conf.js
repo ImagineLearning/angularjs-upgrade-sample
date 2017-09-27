3036c79a8aa577f2364db8917510f885b18a5d15
Eric Gibby <eric.gibby@imaginelearning.com>
2017-09-27 17:15:20 -0600

    Fixed test configuration so AngularJS (ES5) tests work with downgraded Angular component. Added basic TypeScript test running alongside ES5 tests.

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,7 @@
 const config = require('./config');
 const glob = require('./utils').glob;
+const webpackConfig = require('./webpack.config');
+// console.log(JSON.stringify(webpackConfig, null, '\t'));
 
 const buildDir = config.buildDir;
 const files = config.files;
@@ -7,9 +9,13 @@ const files = config.files;
 module.exports = config => {
 	const testFiles = [].concat(files.vendor_js, files.test_vendor_js, [
 		'src/**/*.module.js',
+		'test/**/*.js',
 		'src/**/*.js',
 		'src/**/*.+(spec|test).ts'
 	]);
+
+	let karmaWebpackConfig = Object.assign({}, webpackConfig, { devtool: 'cheap-inline-source-map' });
+	delete karmaWebpackConfig.entry;
 
 	config.set({
 		basePath: './',
@@ -17,17 +23,16 @@ module.exports = config => {
 		exclude: ['src/**/index.js', 'src/app/app.imports.js'],
 		files: testFiles,
 		frameworks: ['jasmine'],
-		plugins: [
-			'karma-chrome-launcher',
-			'karma-jasmine',
-			'karma-spec-reporter',
-			'karma-webpack',
-			'karma-sourcemap-loader'
-		],
 		preprocessors: {
-			'src/**/*.+(spec|test).ts': ['webpack', 'sourcemap']
+			'src/**/*.ts': ['webpack', 'sourcemap']
 		},
 		reporters: ['spec'],
-		singleRun: false
+		singleRun: true,
+		// Need this to load TypeScript tests in Karma
+		mime: {
+			'text/x-typescript': ['ts', 'tsx']
+		},
+		webpack: karmaWebpackConfig,
+		beforeMiddleware: ['webpackBlocker']
 	});
 };
---
+++ test/data-table.module.js
@@ -0,0 +1,9 @@
+/**
+ * Stubbed in AngularJS module for downgraded Angular component.
+ * This is loaded during unit tests to prevent failures.
+ */
+(function() {
+	'use strict';
+
+	angular.module('ng4DataTable', []);
+})();
